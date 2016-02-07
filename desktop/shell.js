2f9ae2b3022751c01f38e69f309e20499d7700cc
Hans Felde <m33pfish@gmail.com>
2016-02-07 00:08:25 -0600

    Update shell.js

--- desktop/shell.js
+++ desktop/shell.js
@@ -1,6 +1,5 @@
 $(".shell-input").onsubmit = function() {
   this.parentElement.insertBefore(div({
-    style: "font-family: monospace;",
     innerHTML: eval(this.value)
   }), this);
   this.value = "";
