23a10fc01d61afec9c5be77197a84a9e6d0a522f
Hans Felde <m33pfish@gmail.com>
2016-02-12 15:47:22 -0600

    Update asdf.js

--- asdf/asdf.js
+++ asdf/asdf.js
@@ -1,3 +1,6 @@
+var canvas = document.querySelector("#frame");
+var ctx = canvas.getContext("2d");
+
 var keysDown = {};
 addEventListener("keydown", e => keysDown[e.keyCode] = true);
 addEventListener("keyup", e => delete keysDown[e.keyCode]);
@@ -5,34 +8,55 @@ addEventListener("keyup", e => delete keysDown[e.keyCode]);
 var player = {
   x: 0,
   y: 0,
+  w: 32,
+  h: 32,
+  color: "#000",
   speedx: 0,
+  speedy: 0,
   accel: 128 / 60,
+  jump: 256,
+  gravity: 4,
   inAir: function() {return this.y > 0},
   jump: function() {
-    
+    this.speedy = this.jump;
   },
   jumpQueued: false
 };
 
-function update() {
-  if (!player.inAir()) {
-    if (player.jumpQueued) {
-      player.jump();
+var update = function(o) {
+  if (!o.inAir()) {
+    if (o.jumpQueued) {
+      o.jump();
     }
     if (37 in keysDown) {
-      player.speedx -= player.accel;
+      o.speedx -= o.accel;
     } else if (37 in keysDown) {
-      player.speedx += player.accel;
+      o.speedx += o.accel;
     } else {
-      player.speedx /= 1.2;
+      o.speedx /= 1.2;
     }
+  } else {
+    o.y += o.speedy - o.gravity;
+    o.speedy /= 1.2;
   }
   if (32 in keysDown) {
-    if (player.inAir) {
-      player.jumpQueued = true;
+    if (o.inAir) {
+      o.jumpQueued = true;
     } else {
-      player.jump();
+      o.jump();
     }
   }
-  player.y = Math.max(0, player.y);
+  o.y = Math.max(0, o.y);
+};
+
+var render = function(o) {
+  ctx.fillStyle = o.color;
+  ctx.fillRect(o.x, canvas.height - o.y - o.h, o.x + o.w, canvas.height - o.y);
+};
+
+var main = function() {
+  update(player);
+  render(player);
+  
+  requestAnimationFrame(main);
 };
