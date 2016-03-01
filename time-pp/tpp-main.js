04ad8f035607409afb1222dcb55137255513fdee
Hans Felde <m33pfish@gmail.com>
2016-03-01 16:02:01 -0600

    Update tpp-main.js

--- time-pp/tpp-main.js
+++ time-pp/tpp-main.js
@@ -1,1 +1,21 @@
+var $ = s => document.querySelector(s)
+var $$ = s => document.querySelectorAll(s)
+var $N = t => p => Object.assign(document.createElement(t), p)
+var $APPEND = ea => eb => ea.appendChild(eb)
 
+var beep = new Audio("./beep.wav")
+var wrap = $("#controls")
+
+var cycle = function(...t) {
+  var go = true
+  t = t.map(n => n * 1000)
+  (function f(t) {
+    beep.play()
+    if (go)
+      setTimeout(function() {
+        f([...t.slice(1), t[0]])
+      })
+  })(t)
+}
+
+[$N("")]
