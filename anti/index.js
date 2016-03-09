f2d24a143f3634f41d461b24f4973d08fe2be9bb
Hans Felde <m33pfish@gmail.com>
2016-03-09 08:29:28 -0600

    Update index.js

--- anti/index.js
+++ anti/index.js
@@ -6,7 +6,50 @@ var fov = 200
 
 const pyth = (a, b) => Math.sqrt(Math.abs(a) * Math.abs(a) + Math.abs(b) * Math.abs(b))
 
-const vertex = (x, y, z) => Object.assign([x, y, z], {
+const color = function(c) {
+  const c = c || 0
+  const a = 255 - (c / 0x01000000 & 0xFF)
+  const r = c >> 0x10 & 0xFF
+  const g = c >> 0x08 & 0xFF
+  const b = c >> 0x00 & 0xFF
+  
+  return {
+    r, g, b, a, c,
+    get string() {
+      return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a / 255})`
+    }
+  }
+}
+const stroke = function(color, w) {
+  return {
+    color,
+    width(z) {
+      return w instanceof function
+        ? w(z)
+        : w
+    }
+  }
+}
+
+const vertex = (f => f(f))(maker => (x, y, z) => Object.assign([x, y, z], {
+  get x() {
+    return this[0]
+  },
+  get y() {
+    return this[1]
+  },
+  get z() {
+    return this[2]
+  },
+  set x(v) {
+    return this[0] = v
+  },
+  set y(v) {
+    return this[1] = v
+  },
+  set z(v) {
+    return this[2] = v
+  }
   add(x, y, z) {
     return vertex(this[0] + x, this[1] + y, this[2] + z)
   },
@@ -17,7 +60,8 @@ const vertex = (x, y, z) => Object.assign([x, y, z], {
     return vertex(this[0] * x, this[1] * y, this[2] * z)
   },
   rotate(a) {
-    return vertex(pyth(this[0], this[2]) * cos(a), this[1], pyth(this[0], this[2]) * sin(a))
+    const angle = Math.atan2(0 - this[2], 0 - this[0])
+    return vertex(pyth(this[0], this[2]) * cos(angle + a), this[1], pyth(this[0], this[2]) * sin(angle + a))
   },
   get inv() {
     return vertex(-this[0], -this[1], -this[2])
@@ -30,38 +74,70 @@ const vertex = (x, y, z) => Object.assign([x, y, z], {
   },
   get copy() {
     return vertex(...this)
-  }
-})
-const vertex2D = (x, y) => Object.assign([x, y], {
+  },
+  maker
+}))
+const vertex2D = (f => f(f))(maker => (x, y) => Object.assign([x, y], {
+  get x() {
+    return this[0]
+  },
+  get y() {
+    return this[1]
+  },
+  set x(v) {
+    return this[0] = v
+  },
+  set y(v) {
+    return this[1] = v
+  },
   add(x, y) {
     return [this[0] + x, this[1] + y]
   },
-  copy: vertex2D(...this)
-})
-const line = (v1, v2) => [v1, v2]
-const lines = (...a) => ({
-  lines: a
-})
-const cube = (c, r) => lines(...[
-  line(center.add(-r, -r, -r), center.add(r, -r, -r)),
-  line(center.add(-r, -r, -r), center.add(-r, -r, r)),
-  line(center.add(r, -r, -r), center.add(r, -r, r)),
-  line(center.add(-r, -r, r), center.add(r, -r, r)),
+  copy: vertex2D(...this),
+  maker
+}))
+const line = (f => f(f))(maker => (v1, v2, stroke_) => Object.assign([v1, v2], {
+  stroke: stroke_ || stroke(color(), 1),
+  maker
+}))
+const lines = (f => f(f))(maker => (stroke, ...a) => ({
+  lines: a.map(o => o.stroke = stroke),
+  maker
+}))
+const poly = (f => f(f))(maker => (fill, stroke, ...a) => ({
+  fill, stroke,
+  vertices: a,
+  maker
+}))
+const cube = (s, c, r) => lines(s, ...[
+  line(c.add(-r, -r, -r), c.add(r, -r, -r)),
+  line(c.add(-r, -r, -r), c.add(-r, -r, r)),
+  line(c.add(r, -r, -r), c.add(r, -r, r)),
+  line(c.add(-r, -r, r), c.add(r, -r, r)),
   
-  line(center.add(-r, -r, -r), center.add(-r, r, -r)),
-  line(center.add(r, -r, -r), center.add(r, r, -r)),
-  line(center.add(-r, -r, r), center.add(-r, r, r)),
-  line(center.add(r, -r, r), center.add(r, r, r)),
+  line(c.add(-r, -r, -r), c.add(-r, r, -r)),
+  line(c.add(r, -r, -r), c.add(r, r, -r)),
+  line(c.add(-r, -r, r), c.add(-r, r, r)),
+  line(c.add(r, -r, r), c.add(r, r, r)),
   
-  line(center.add(-r, r, -r), center.add(r, r, -r)),
-  line(center.add(-r, r, -r), center.add(-r, r, r)),
-  line(center.add(r, r, -r), center.add(r, r, r)),
-  line(center.add(-r, r, r), center.add(r, r, r))
+  line(c.add(-r, r, -r), c.add(r, r, -r)),
+  line(c.add(-r, r, -r), c.add(-r, r, r)),
+  line(c.add(r, r, -r), c.add(r, r, r)),
+  line(c.add(-r, r, r), c.add(r, r, r))
 ])
 
-const camera = (x, y, z, angle, fov) => ({
+const camera = (f => f(f))(maker => (x, y, z, angle, fov) => ({
   x, y, z, angle, fov,
   project(v) {
     return v.sub(this.x, this.y, this.z).rotate(this.angle)
+  },
+  maker
+}))
+
+const draw = c => ctx => o => {
+  switch (o.maker) {
+    case vertex:
+      
+      break;
   }
-})
+}
